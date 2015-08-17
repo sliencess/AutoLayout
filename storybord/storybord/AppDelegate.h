2c4456faedf898061a0828d832b112df975ed1c0
quandanhui <quandanhui@icloud.com>
2015-08-17 18:56:01 +0800

    first commit

---
+++ storybord/storybord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  storybord
+//
+//  Created by user_ on 15/8/14.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
